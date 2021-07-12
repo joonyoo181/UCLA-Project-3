352669a5af7c81e72a138a68669846ab9980881b
bluev <joon.yoo181@gmail.com>
2021-07-12 15:05:10 -0700

    testing

--- Project 3/Source.cpp
+++ Project 3/Source.cpp
@@ -12,37 +12,26 @@ int endingFloor(string elevatorstring);
 int highestFloor(string elevatorstring);
 
 int main() {
-    string s = "Helo 123 My name is..";
-    size_t t = 5;
-    cout << extractNumber(s, t);
 }
 
-int extractNumber(string s, size_t & index)
-{
-    // track the integer value found
+int extractNumber(string s, size_t & index){
     int quantity(0);
     bool foundADigit = false;
 
-    while (index < s.size())
-    {
+    while (index < s.size()){
         char c = s.at(index);
-        // we are only interested in digit characters 0-9
-        if (c >= '0' && c <= '9')
-        {
+
+        if (c >= '0' && c <= '9'){
             foundADigit = true;
-            // extract one digit and add it to the cumulative 
-            // value held in quantity
             int digit = c - '0';
             quantity = quantity * 10 + digit;
             index = index + 1;
         }
-        else
-        {
+        else{
             break;
         }
     }
-    if (!foundADigit)
-    {
+    if (!foundADigit){
         quantity = -1;
     }
     return(quantity);
