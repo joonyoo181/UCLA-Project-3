5f5341022da93296438b768752076b4b084472e7
bluev <joon.yoo181@gmail.com>
2021-07-14 12:17:54 -0700

    finished making 6 functions; now on the testing phase, but error on line 26

--- Project 3/Source.cpp
+++ Project 3/Source.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int extractNumber(string s, size_t& index);
+int extractNumber(string s, size_t index);
 
 bool isValidElevatorString(string elevatorstring);
 bool doorsOpen(string elevatorstring);
@@ -13,13 +13,22 @@ int endingFloor(string elevatorstring);
 int highestFloor(string elevatorstring);
 
 int main() {
-    assert(isValidElevatorString("M") == false);
-    assert(isValidElevatorString("M1C") == false);
-    assert(isValidElevatorString("M1OCOO") == false);
-    assert(isValidElevatorString("M1O-12+50") == false);
-    assert(isValidElevatorString("M1O+4-3-1CM2O-1") == false);
-    assert(isValidElevatorString("M1O+-C") == false);
-    assert(isValidElevatorString("M-3") == false);
+    assert(isValidElevatorString("") == false);
+    assert(isValidElevatorString("    ") == false);
+    assert(doorsOpen("    ") == false);
+    assert(endingPassengers("      ") == -1);
+    assert(mostPassengers("      ") == -1);
+    assert(endingFloor("       ") == -1);
+    assert(highestFloor("       ") == -1);
+    assert(isValidElevatorString("M1O+3-2C"));
+    assert(doorsOpen("M1O+3-2C") == false);
+    assert(endingPassengers("M1O+3-2C") == 1);
+    assert(mostPassengers("M1O+3-2C") == 3);
+    assert(endingFloor("M1O+3-2C") == 1);
+    assert(highestFloor("M1O+3-2C") == 1);
+
+    cout << "All tests succeeded" << endl;
+    return 0;
 }
 
 int extractNumber(string s, size_t index){
@@ -48,123 +57,255 @@ int extractNumber(string s, size_t index){
 bool isValidElevatorString(string elevatorstring) {
     string str = elevatorstring;
 
-    //check if the elevator string begin with M followed by digits
-    cout << "check if the elevator string begin with M followed by digits" << endl;
-    if (str.at(0) == 'M') {
-        cout << "in the if statement" << endl;
-        if (str.at(1) >= '0' && str.at(1) <= '9') {
-            cout << "in the second if statement" << endl;
-            //emptied for readability
-        }
-        else {
-            cout << "return at line 62" << endl;
-            return false;
-        }
-    }
-    else {
-        cout << "return at line 67" << endl;
-        return false;
-    }
+    //make sure the string is more than the length of 1
+    if (str.length() >= 2) {
 
-    char c;
-    bool isOpen = false;
-    int people = 0;
-    for (size_t pos = 0; pos < str.length(); pos++) {
-        c = str.at(pos);
+        char c;
+        bool isOpen = false;
+        int people = 0;
 
-        //check if the only characters present are M, O, C, +, -, and digit chars
-        cout << "check if the only characters present are M, O, C, +, -, and digit chars" << endl;
-        if (c < '+' || c == 44 || (c > '-' && c < '0') || 
-            (c > '9' && c < 'C') || (c > 'C' && c < 'M') || 
-            (c > 'M' && c < 'O') || c > 'O') {
-            cout << "return at line 81" << endl;
-            return false;
-        }
+        for (size_t pos = 0; pos < str.length(); pos++) {
+            c = str.at(pos);
 
-        //check that when the elevator moves, the door is closed
-        cout << "check that when the elevator moves, the door is closed" << endl;
-        if (c == 'M'){
-            if (!isOpen) {
-                //check that when the elevator moves, it is followed by a number
-                cout << "check that when the elevator moves, it is followed by a number" << endl;
-                if (str.at(pos + 1) >= '0' && str.at(pos + 1) <= '9') {
-                    //emptied for readability
+            if (pos == 0) {
+                if (c == 'M') {
+                    //continue
                 }
                 else {
-                    cout << "return at line 92" << endl;
                     return false;
                 }
             }
-            else {
-                cout << "return at line 97" << endl;
+
+            //check if the only characters present are M, O, C, +, -, and digit chars
+            if (c < '+' || c == 44 || (c > '-' && c < '0') ||
+                (c > '9' && c < 'C') || (c > 'C' && c < 'M') ||
+                (c > 'M' && c < 'O') || c > 'O') {
+                return false;
+            }
+
+            //check that when the elevator moves, the door is closed
+            if (c == 'M') {
+                if (!isOpen) {
+                    //check that when the elevator moves, it is followed by a number
+                    if (str.at(pos + 1) > '0' && str.at(pos + 1) <= '9') {
+                        //emptied for readability
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+
+            //check to see that the door was closed before opening
+            if (c == 'O') {
+                if (!isOpen) {
+                    isOpen = true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            //check to see that the door was opened before closing
+            if (c == 'C') {
+                if (isOpen) {
+                    isOpen = false;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            //if there is a plus sign, then check to see that the door is open
+            if (c == '+') {
+
+                //update number of ppl in the elevator
+                if (isOpen) {
+                    people += extractNumber(str, pos + 1);
+                }
+                else {
+                    return false;
+                }
+            }
+
+            //if there is a minus sign, then check to see that the door is open
+            if (c == '-') {
+
+                //update number of ppl in the elevator
+                if (isOpen) {
+                    people -= extractNumber(str, pos + 1);
+                }
+                else {
+                    return false;
+                }
+            }
+
+            //check if there is negative number of ppl in elevator
+            if (people < 0) {
                 return false;
             }
         }
 
-        //check to see that the door was closed before opening
-        cout << "check to see that the door was closed before opening" << endl;
-        if (c == 'O'){
-            if (!isOpen) {
-                isOpen = true;
-            }
-            else {
-                cout << "return at line 107" << endl;
-                return false;
-            }
+        //check that the string ends with 'O', 'C', or a digit character
+        char lastChar = str.at(str.length() - 1);
+        if (lastChar == 'O' || lastChar == 'C' || (lastChar >= '0' && lastChar <= '9')) {
+            //emptied for readability
         }
-
-        //check to see that the door was opened before closing
-        cout << "check to see that the door was opened before closing" << endl;
-        if (c == 'C'){
-            if (isOpen) {
-                isOpen = false;
-            }
-            else {
-                cout << "return at line 118" << endl;
-                return false;
-            }
-        }
-
-        //if there is a plus sign, then check to see that the door is open
-        cout << "if there is a plus sign, then check to see that the door is open" << endl;
-        if (c == '+'){
-            if (isOpen) {
-                people += extractNumber(str, pos + 1);
-            }
-            else {
-                cout << "return at line 124" << endl;
-                return false;
-            }
-        }
-
-        //if there is a minus sign, then check to see that the door is open
-        cout << "if there is a minus sign, then check to see that the door is open" << endl;
-        if (c == '-' ){
-            if (isOpen) {
-                people -= extractNumber(str, pos + 1);
-            }
-            else {
-                cout << "return at line 133" << endl;
-                return false;
-            }
-        }
-
-        //check if there is negative number of ppl in elevator
-        cout << "check if there is negative number of ppl in elevator" << endl;
-        if (people < 0) {
-            cout << "return at line 139" << endl;
+        else {
             return false;
         }
     }
-
-    char lastChar = str.at(str.length() - 1);
-    if (lastChar == 'O' || lastChar == 'C' || (lastChar >= '0' && lastChar <= '9')) {
-        //emptied for readability
-    }
     else {
-        cout << "return at line 149" << endl;
         return false;
     }
 
     return true;
+}
+
+bool doorsOpen(string elevatorstring) {
+    string str = elevatorstring;
+
+    bool isOpen = false;
+
+    //check if valid string
+    if (isValidElevatorString(str)) {
+        char c;
+
+        //iterating thru each char and changeing the isOpen variable
+        for (size_t pos = 0; pos < str.length(); pos++) {
+            c = str.at(pos);
+
+            if (c == 'O') {
+                isOpen = true;
+            }
+            if (c == 'C') {
+                isOpen = false;
+            }
+        }
+    }
+
+    return isOpen;
+}
+
+int endingPassengers(string elevatorstring) {
+    string str = elevatorstring;
+
+    int people = 0;
+
+    //check if valid string
+    if (isValidElevatorString(str)) {
+        char c;
+
+        //iterating thru each char and updating the number of ppl onboard
+        for (size_t pos = 0; pos < str.length(); pos++) {
+            c = str.at(pos);
+
+            if (c == '+') {
+                people += extractNumber(str, pos+1);
+            }
+            if (c == '-') {
+                people -= extractNumber(str, pos + 1);
+            }
+        }
+    }
+    else {
+        return -1;
+    }
+
+    return people;
+}
+
+int mostPassengers(string elevatorstring) {
+    string str = elevatorstring;
+
+    int max_ppl = INT_MIN;
+    int people = 0;
+
+    //check if valid string
+    if (isValidElevatorString(str)) {
+        char c;
+
+        //iterating thru each char and updating number of ppl onboard
+        for (size_t pos = 0; pos < str.length(); pos++) {
+            c = str.at(pos);
+
+            if (c == '+') {
+                people += extractNumber(str, pos + 1);
+            }
+            if (c == '-') {
+                people -= extractNumber(str, pos + 1);
+            }
+
+            //if the current number of ppl onboard is greater than the previous greatest number, set a new greatest number
+            if (people > max_ppl) {
+                max_ppl = people;
+            }
+        }
+    }
+    else {
+        return -1;
+    }
+
+    return people;
+}
+
+int endingFloor(string elevatorstring) {
+    string str = elevatorstring;
+
+    int floor = 0;
+
+    //check if valid string
+    if (isValidElevatorString(str)) {
+        char c;
+
+        //iterating thru each char and set floor value
+        for (size_t pos = 0; pos < str.length(); pos++) {
+            c = str.at(pos);
+
+            //set the floor variable as the number that follows 'M'
+            if (c == 'M') {
+                floor = extractNumber(str, pos + 1);
+            }
+        }
+    }
+    else {
+        return -1;
+    }
+
+    return floor;
+}
+
+int highestFloor(string elevatorstring) {
+    string str = elevatorstring;
+
+    int max_floor = INT_MIN;
+    int floor = 0;
+
+    //check if valid string
+    if (isValidElevatorString(str)) {
+        char c;
+
+        //iterating thru each char until it reaches 'M'
+        for (size_t pos = 0; pos < str.length(); pos++) {
+            c = str.at(pos);
+
+            //set the floor variable
+            if (c == 'M') {
+                floor = extractNumber(str, pos + 1);
+            }
+
+            //if the current floor variable is larger than the max_floor variable, set max_floor to current floor.
+            if (floor > max_floor) {
+                max_floor = floor;
+            }
+        }
+    }
+    else {
+        return -1;
+    }
+
+    return max_floor;
 }
